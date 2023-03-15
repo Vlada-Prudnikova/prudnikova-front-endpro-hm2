a3129d72e0d4a7f4cabce0c80faaf3a8ebe19f4a
Влада <vlada.prudnikova.2004@gmail.com>
2023-03-15 13:22:03 -0400

    Intial commit

---
+++ first.js
@@ -0,0 +1,8 @@
+let firstString = " Hi guys!";
+let secondString = " Today I wanna show you";
+let thirdString = " my morning routine";
+let joined = firstString + secondString + thirdString;
+console.log(joined);
+
+let number = "12345";
+console.log(" 1 2 3 4 5 ");
